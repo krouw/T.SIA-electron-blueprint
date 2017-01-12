df52930c49b97001c592e97ec182a20b67338d82
krouw <felipe.andrez@live.cl>
2017-01-11 21:34:58 -0300

    Agregando contador servidor

---
+++ server/api/controllers/ContadorController.js
@@ -0,0 +1,11 @@
+/**
+ * ContadorController
+ *
+ * @description :: Server-side logic for managing contadors
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
