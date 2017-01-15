21e18ba127ea44884edc214ad579a05e25725cb5
krouw <felipe.andrez@live.cl>
2017-01-15 19:13:02 -0300

    HistorialFilter Component

--- client/app/components/HistorialFilter/HistorialFilter.js
+++ client/app/components/HistorialFilter/HistorialFilter.js
@@ -3,8 +3,14 @@ import { Row, Col } from 'react-flexbox-grid';
 import {
   Popover,
   PopoverInteractionKind,
-  Position} from '@blueprintjs/core';
+  Position,
+  Tag,
+  Classes,
+  Intent } from '@blueprintjs/core';
 import { DateRangePicker } from "@blueprintjs/datetime";
+import SearchInput from 'react-search-input'
+import moment from 'moment';
+import classNames from 'classnames';
 
 export default class HistorialFilter extends Component {
   constructor(props){
@@ -14,45 +20,61 @@ export default class HistorialFilter extends Component {
     }
   }
 
-  onChange(e){
-    this.setState({[e.target.name]: e.target.value});
+  onChangeSearch(e){
+    this.setState({search: e});
+    this.props.searchUpdated(e);
+  }
+
+  handleDateChange(dateRange){
+    this.props.handleDateChange(dateRange);
   }
 
   render() {
     let popoverContent = (
             <div>
-              <DateRangePicker/>
+              <DateRangePicker
+                onChange={(dateRange) => this.handleDateChange(dateRange)}
+              />
             </div>
         );
+    const Moment = (date) =>{
+      if(date){
+          return (<Tag className={Classes.LARGE}
+          intent={Intent.PRIMARY}>
+          {moment(date).format("D MMMM YYYY")}
+        </Tag>)
+      }
+      else{
+        return (<Tag
+          className={classNames(Classes.LARGE, Classes.MINIMAL)}>
+          Sin Fecha
+        </Tag>)
+      }
+    }
+
     return (
       <Row start="xs" bottom="sm" className="Historial-Filter">
         <Col xs={12} sm={4}>
           <label className="pt-label">
             Buscar
-            <input
-             name="search"
-             className="pt-input"
+            <SearchInput
+             inputClassName="pt-input"
              type="text"
-             dir="auto"
-             onChange={ (e) => this.onChange(e) }
+             onChange={ e => this.onChangeSearch(e) }
              value={this.state.search}
             />
           </label>
         </Col>
         <Col xs={12} sm={8}>
           <Popover content={popoverContent}
-                   interactionKind={PopoverInteractionKind.CLICK}
-                   popoverClassName="DatePicker pt-popover-content-sizing"
-                   position={Position.BOTTOM}
-                   useSmartPositioning={false}>
+                  interactionKind={PopoverInteractionKind.CLICK}
+                  popoverClassName="DatePicker pt-popover-content-sizing"
+                  position={Position.BOTTOM}
+                  useSmartPositioning={false}>
             <div className="Historial-Fecha">
-              <span className="pt-tag pt-large pt-minimal">
-                Fecha Incio
-              </span>
+              { Moment(this.props.dateRange[0]) }
               <span className="pt-icon-large pt-icon-arrow-right"></span>
-              <span className="pt-tag pt-large pt-minimal">
-                Fecha Termino
-              </span>
+              { Moment(this.props.dateRange[1]) }
             </div>
           </Popover>
           <button type="button" className="pt-button pt-icon-document pt-intent-success">Generar Reporte</button>
@@ -60,4 +82,9 @@ export default class HistorialFilter extends Component {
       </Row>
     )
   }
+}
+
+HistorialFilter.propsTypes = {
+  searchUpdated: React.PropTypes.func.isRequired,
+  handleDateChange: React.PropTypes.func.isRequired,
 }
--- client/app/containers/Historial/Historial.js
+++ client/app/containers/Historial/Historial.js
@@ -1,214 +1,22 @@
 import React, {Component} from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
-import Dialog from 'material-ui/Dialog';
-import {deepOrange500} from 'material-ui/styles/colors';
-import FlatButton from 'material-ui/FlatButton';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MenuItem from 'material-ui/MenuItem';
-import {Card, CardHeader} from 'material-ui/Card';
-import IconButton from 'material-ui/IconButton';
-import PersonAdd from 'material-ui/svg-icons/social/person-add';
-import InfoOutline from 'material-ui/svg-icons/action/info-outline';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import axios from 'axios';
-import moment from 'moment';
-
-import DataTables from 'material-ui-datatables';
+import SearchInput, {createFilter} from 'react-search-input'
 import HistorialFilter from '../../components/HistorialFilter/HistorialFilter'
+import axios from 'axios'
+import moment from 'moment'
 
-injectTapEventPlugin();
+const KEYS_TO_FILTERS = ['fecha', 'rut', 'nombre','rol','asignatura']
 
-const styles = {
-  container: {
-    textAlign: 'center',
-  }
-};
-
-const muiTheme = getMuiTheme({
-  palette: {
-    accent1Color: deepOrange500,
-  },
-});
-
-const TABLE_COLUMNS = [
-  {
-    key: 'fecha',
-    label: 'Fecha',
-  }, {
-    key: 'rut',
-    label: 'Rut',
-  }, {
-    key: 'nombre',
-    label: 'Nombre',
-  }, {
-    key: 'rol',
-    label: 'Rol',
-  },{
-    key: 'asignatura',
-    label: 'Asignatura',
-  }, {
-    key: 'observacion',
-    label: 'Observacion',
-  }, {
-    key: 'hojas',
-    label: 'Hojas',
-  },
-
-];
-
-const TABLE_COLUMNS_TOOLTIP = [
-  {
-    key: 'fecha',
-    label: 'Fecha',
-    tooltip: 'Fecha',
-  }, {
-    key: 'rut',
-    label: 'Rut',
-    tooltip: 'Rut',
-  }, {
-    key: 'nombre',
-    label: 'Nombre',
-    tooltip: 'Nombre',
-  }, {
-    key: 'rol',
-    label: 'Rol',
-    tooltip: 'Rol',
-  },{
-    key: 'asignatura',
-    label: 'Asignatura',
-    tooltip: 'Asignatura',
-  }, {
-    key: 'observación',
-    label: 'Obervacion',
-    tooltip: 'Observacion',
-  }, {
-    key: 'hojas',
-    label: 'Hojas',
-    tooltip: 'Hojas',
-  },
-];
-
-const TABLE_COLUMNS_SORT_STYLE = [
-  {
-    key: 'fecha',
-    label: 'Fecha',
-    sortable: true,
-    style: {
-      width: 80,
-    }
-  }, {
-    key: 'rut',
-    label: 'Rut',
-    sortable: true,
-    style: {
-      width: 85,
-    }
-  }, {
-    key: 'nombre',
-    label: 'Nombre',
-    style: {
-      width: 155,
-    }
-  }, {
-    key: 'rol',
-    label: 'Rol',
-    style: {
-      width: 80,
-    }
-  }, {
-    key: 'asignatura',
-    label: 'Asignatura',
-    style: {
-      width: 110,
-      textAlign: 'center',
-    }
-  },
-  {
-    key: 'observacion',
-    label: 'Obervacion',
-    style: {
-      width: 130,
-    }
-  }, {
-    key: 'hojas',
-    label: 'Hojas',
-    style: {
-      width: 80,
-      textAlign: 'center',
-    }
-  }
-];
-
-
-
-const TABLE_DATA_NEXT = [
-  {
-
-  }
-];
-
-const TABLE_DATA = [
-  {
-    fecha: '20/01/2017',
-    rut: '18082418-9',
-    nombre: 'Carlos Riquelme Labrin ',
-    rol: 'estudiante',
-    asignatura: 'programacion',
-    observacion: 'tarea arboles binarios',
-    hojas: '10',
-  },
-  {
-    fecha: '21/01/2017',
-    rut: '18082418-9',
-    nombre: 'felipe rubio',
-    rol: 'estudiante',
-    asignatura: 'programacion',
-    observacion: 'tarea arboles binarios',
-    hojas: '10',
-  },
-  {
-    fecha: '22/01/2017',
-    rut: '18082418-9',
-    nombre: 'tomas riquelme',
-    rol: 'estudiante',
-    asignatura: 'programacion',
-    observacion: 'tarea arboles binarios',
-    hojas: '10',
-  },
-  {
-    fecha: '20/01/2017',
-    rut: '18082418-9',
-    nombre: 'Carlos Riquelme Labrin ',
-    rol: 'estudiante',
-    asignatura: 'programacion',
-    observacion: 'tarea arboles binarios',
-    hojas: '10',
-  }
-];
-
-class Historial extends Component {
-  constructor(props, context) {
-    super(props, context);
-/*
-    this.handleSortOrderChange = this.handleSortOrderChange.bind(this);
-    //this.handleFilterValueChange = this.handleFilterValueChange.bind(this);
-    this.handleCellClick = this.handleCellClick.bind(this);
-    this.handleCellDoubleClick = this.handleCellDoubleClick.bind(this);
-    this.handleRowSelection = this.handleRowSelection.bind(this);
-    this.handlePreviousPageClick = this.handlePreviousPageClick.bind(this);
-    this.handleNextPageClick = this.handleNextPageClick.bind(this);
-    this.handlePersonAddClick = this.handlePersonAddClick.bind(this);
-    this.handleInfoClick = this.handleInfoClick.bind(this);
-*/
+class Historial extends Component{
+  constructor(props){
+    super(props);
     this.state = {
-      impresions: [],
-      page: 1,
-    };
-
+      searchTerm: '',
+      impresiones: [],
+      dateRange: [],
+    }
   }
 
-  loadHistorial(){
+  componentDidMount(){
     const allImpresiones = [];
     axios.get('http://localhost:1337/impresion?sort=updatedAt DESC')
       .then((response) => {
@@ -222,92 +30,41 @@ class Historial extends Component {
             asignatura: impresion.asignatura,
             observacion: impresion.observacion,
             hojas: impresion.cantidad,
+            id: impresion.id,
           }
         allImpresiones.push(aux);
         });
-
-    });
-
-    this.setState({impresions: allImpresiones})
-    console.log(allImpresiones);
-  }
-  componentWillMount(){
-      this.loadHistorial();
-  }
-/*
-  handleSortOrderChange(key, order) {
-    console.log('key:' + key + ' order: ' + order);
-  }
-
-  handleFilterValueChange(e) {
-    console.log('filter value: ' + value);
-    this.setState({[e.target.search]: e.target.value});
-  }*/
-/*
-  handleCellClick(rowIndex, columnIndex, row, column) {
-    console.log('rowIndex: ' + rowIndex + ' columnIndex: ' + columnIndex);
-  }
-
-  handleCellDoubleClick(rowIndex, columnIndex, row, column) {
-    console.log('rowIndex: ' + rowIndex + ' columnIndex: ' + columnIndex);
-  }
-
-  handleRowSelection(selectedRows) {
-    console.log('selectedRows: ' + selectedRows);
-  }
-
-  handlePreviousPageClick() {
-    console.log('handlePreviousPageClick');
-    this.setState({
-      data: todasImpresiones,
-      page: 1,
+        this.setState({impresiones: allImpresiones})
     });
   }
 
-  handleNextPageClick() {
-    console.log('handleNextPageClick');
-    this.setState({
-      data: TABLE_DATA_NEXT,
-      page: 2,
-    });
+  searchUpdated (term) {
+    this.setState({searchTerm: term});
   }
 
-  handlePersonAddClick() {
-    console.log('handlePersonAddClick');
+  handleDateChange(dateRange){
+    this.setState({dateRange: dateRange});
   }
 
-  handleInfoClick() {
-    console.log('handleInfoClick');
-  }
-*/
-  render() {
-
-    let prueba = new Date();
+  render(){
+      const filteredImpre = this.state.impresiones.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
     return (
-      <section className="Historial">
-        <HistorialFilter />
-        <MuiThemeProvider muiTheme={muiTheme}>
-          <div style={styles.component}>
-            <Card>
-              <DataTables
-                height={'auto'}
-                selectable={false}
-                showRowHover={true}
-                columns={TABLE_COLUMNS_SORT_STYLE}
-                data={this.state.impresions}
-                showCheckboxes={false}
-                showHeaderToolbar={false}
-                //onCellClick={this.handleCellClick}
-                //onCellDoubleClick={this.handleCellDoubleClick}
-                //onFilterValueChange={this.handleFilterValueChange}
-                //onSortOrderChange={this.handleSortOrderChange}
-                count={3}
-              />
-            </Card>
-          </div>
-        </MuiThemeProvider>
-      </section>
-    );
+      <div>
+        <HistorialFilter
+          searchUpdated={term => this.searchUpdated(term)}
+          handleDateChange={dateRange => this.handleDateChange(dateRange)}
+          dateRange={this.state.dateRange}
+        />
+        {filteredImpre.map( impre => {
+          return (
+            <ul className="mail" key={impre.id}>
+              <li>{impre.nombre}</li>
+              <li>{impre.rut}</li>
+            </ul>
+          )
+        })}
+      </div>
+    )
   }
 }
 
